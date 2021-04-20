251e7e1e3ad98ba62d8c737fc7b6178d6e6e5cb8
Inzemam ul-Haq <33024370+ishqDehlvi@users.noreply.github.com>
2021-04-20 19:25:30 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+
+Instagram Auto-Posting BOT - Post Automatically to instagram (Every X minutes)
---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+$username = 'INSTAGRAM_USERNAME';
+$password = 'INSTAGRAM_PASSWORD';
+
+$image_description = 'Your Description Here ...'; 
+$imgurl = 'https://picsum.photos/700/?random';
+$photoFilename = "img/rand.jpg";
